38e8cee40714af00c8a2a33ce9030740c22668b3
MostafaSensei106 <wwwmostafagmer100'gmail.com>
2026-02-25 21:50:58 +0200

    - feat(packaging): Add ios and macOS support to support the Swift Package Manager

---
+++ ios/network_reachability/Package.swift
@@ -0,0 +1,42 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = package(
+    name: "network_reachability",
+    platforms:[
+        .iOS("13.0"),
+        .macOS("10.15")
+    ],
+
+      products: [
+        // TODO: Update your library and target names.
+        // If the plugin name contains "_", replace with "-" for the library name.
+        .library(name: "network-reachability", targets: ["network_reachability"]),
+    ],
+
+    dependencies: [],
+
+     targets: [
+        .target(
+            // TODO: Update your target name.
+            name: "network_reachability",
+            dependencies: [],
+            resources: [
+                // TODO: If your plugin requires a privacy manifest
+                // (e.g. if it uses any required reason APIs), update the PrivacyInfo.xcprivacy file
+                // to describe your plugin's privacy impact, and then uncomment this line.
+                // For more information, see:
+                // https://developer.apple.com/documentation/bundleresources/privacy_manifest_files
+                // .process("PrivacyInfo.xcprivacy"),
+
+                // TODO: If you have other resources that need to be bundled with your plugin, refer to
+                // the following instructions to add them:
+                // https://developer.apple.com/documentation/xcode/bundling-resources-with-a-swift-package
+            ]
+        )
+    ]
+    
+
+)
